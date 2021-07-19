3e96da32cb0fb3a8424330fdff5c30176072ff55
chiragsardana <30770247+chiragsardana@users.noreply.github.com>
2021-07-19 13:47:46 +0530

    ReactJs Chat App

---
+++ src/components/Chats.js
@@ -0,0 +1,94 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
+
+import axios from 'axios';
+
+import { auth } from '../firebase';
+import { useAuth } from '../contexts/AuthContext';
+
+const Chats = () => {
+
+	const [loading, setLoading] = useState(true);
+
+	const history = useHistory();
+	const { user } = useAuth();
+
+	console.log(user);
+
+	const handleLogout = async () => {
+		await auth.signOut();
+		history.push('/');
+	}
+
+	const getFile = async (url) => {
+		const response = await fetch(url);
+		const data = await response.blob();
+
+		return new File([data], "userPhoto.jpg", { type: 'image/jpeg' });
+	}
+
+	useEffect(() => { 
+		if(!user) {
+			history.push('/');
+
+			return;
+		}
+
+		axios.get('https://api.chatengine.io/users/me/', {
+			headers: {
+				"project-id": "",
+				"user-name": user.email,
+				"user-secret": user.uid
+			}
+		})
+		.then(() => {
+			setLoading(false);
+		})
+		.catch(() => {
+			let formData = new FormData();
+			formData.append('email', user.email);
+			formData.append('username', user.email);
+			formData.append('secret', user.uid);
+
+			getFile(user.photoURL)
+				.then((avatar) => {
+					formData.append('avatar', avatar, avatar.name);
+
+					axios.post('https://api.chatengine.io/users/', formData, 
+					{ headers: { "private-key":  "" }
+					}).then(() => setLoading(false))
+						.catch((error) => console.log(error))
+				
+				})
+		})
+	}, [user, history])
+
+	if(!user || loading) return 'Loading...';
+
+	return (
+		<div className="chats-page">
+			<div className="nav-bar">
+
+				<div className="logo-tab">
+					Unichat
+				</div>
+
+				<div onClick={handleLogout} className="logout-tab">
+					Logout
+				</div>
+
+			</div>
+
+			<ChatEngine 
+				height="calc(100vh - 66px)"
+				projectID={""}
+				userName={user.email}
+				userSecret={user.uid}
+			/>
+
+		</div>
+	);
+}
+
+export default Chats;
